bb4672f359e06d817e383e76052c073c38a4f0ec
ankitaamalap <ankitamalap94@gmail.com>
2023-07-13 12:05:01 +0530

    change color

---
+++ app/src/main/java/com/ankita/changecolor/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.ankita.changecolor
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var view: View
+    private lateinit var button: FloatingActionButton
+    private val colors =
+        arrayOf(Color.BLUE, Color.CYAN, Color.GRAY, Color.MAGENTA, Color.WHITE)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        view = findViewById(R.id.view)
+        button = findViewById(R.id.button_change)
+
+        button.setOnClickListener {
+            view.setBackgroundColor(colors[Random.nextInt(colors.size)])
+        }
+    }
+}
